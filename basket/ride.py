3ea52629eea0e25ef0997e461bb0eb511fa852af
Aaron Ang <aaron.ang@parc.com>
2018-01-06 01:18:50 -0800

    Improve readability of Ride

--- basket/ride.py
+++ basket/ride.py
@@ -1,8 +1,11 @@
 from datetime import datetime
 from typing import NamedTuple, Dict
 
+Item = str
+Count = int
+
 
 class Ride(NamedTuple):
     start_time: datetime
     end_time: datetime
-    items: Dict[str, int]
+    items: Dict[Item, Count]
