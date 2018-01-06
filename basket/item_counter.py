ffd740e057ebc8ddf51670e67fc0a8280eb16812
Aaron Ang <aaron.ang@parc.com>
2018-01-06 00:58:31 -0800

    Implement ItemCounter.process_ride

--- basket/item_counter.py
+++ basket/item_counter.py
@@ -1,3 +1,17 @@
+from typing import List
+
+from basket.ride import Ride
+
+
 class ItemCounter:
-    def count(self):
-        return 1
+    _rides: List[Ride]
+
+    def __init__(self) -> None:
+        self._rides = []
+
+    @property
+    def rides(self) -> List[Ride]:
+        return self._rides
+
+    def process_ride(self, ride: Ride) -> None:
+        self._rides.append(ride)
--- basket/tests/test_item_counter.py
+++ basket/tests/test_item_counter.py
@@ -1,5 +1,39 @@
+from datetime import datetime
+
+import pytest
+
 from basket.item_counter import ItemCounter
+from basket.ride import Ride
 
 
-def test_hello():
-    assert ItemCounter().count() == 1
+@pytest.fixture
+def item_counter():
+    return ItemCounter()
+
+
+def test_item_counter(item_counter):
+    assert len(item_counter.rides) == 0
+
+
+def test_process_ride(item_counter):
+    ride_1 = Ride(
+        start_time=datetime(2017, 1, 5, 7),
+        end_time=datetime(2017, 1, 5, 7, 30),
+        items={
+            'apple': 2,
+            'brownie': 1,
+        })
+
+    ride_2 = Ride(
+        start_time=datetime(2017, 1, 5, 7, 10),
+        end_time=datetime(2017, 1, 5, 8),
+        items={
+            'apple': 1,
+            'carrot': 3,
+        })
+
+    item_counter.process_ride(ride_1)
+    assert len(item_counter.rides) == 1
+
+    item_counter.process_ride(ride_2)
+    assert len(item_counter.rides) == 2
