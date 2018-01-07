755a23cdf87f9cc97099b702359f6cf743036d71
Aaron Ang <aaron.ang@parc.com>
2018-01-06 23:41:08 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from pipenv.project import Project
+from pipenv.utils import convert_deps_to_pip
+from setuptools import setup, find_packages
+
+pipfile = Project(chdir=False).parsed_pipfile
+requirements = convert_deps_to_pip(pipfile['packages'], r=False)
+dev_requirements = convert_deps_to_pip(pipfile['dev-packages'], r=False)
+
+setup(
+    name='item-counter',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=requirements,
+    extras_require={
+        'dev': dev_requirements
+    })
